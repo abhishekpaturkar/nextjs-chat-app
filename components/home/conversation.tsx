2ec2e96bd2f7081cae05e5f04907a14e27349a30
abhishekpaturkar <godkillop03@gmail.com>
2024-06-25 12:26:36 +0530

    Conversation bug Fixedgit add .!

--- components/home/conversation.tsx
+++ components/home/conversation.tsx
@@ -11,18 +11,18 @@ const Conversation = ({ conversation }: { conversation: any }) => {
   const conversationName = conversation.groupName || conversation.name
   const lastMessage = conversation.lastMessage
   const lastMessageType = lastMessage?.messageType
-  const { selectedConversation, setSelectedConversation } =
-    useConversationStore()
   const me = useQuery(api.users.getMe)
 
+  const { setSelectedConversation, selectedConversation } =
+    useConversationStore()
   const activeBgClass = selectedConversation?._id === conversation._id
 
   return (
     <>
       <div
-        className={`flex gap-2 items-center p-3 hover:bg-chat-hover cursor-pointer 
-            ${activeBgClass ? "bg-gray-tertiary" : ""}
-          `}
+        className={`flex gap-2 items-center p-3 hover:bg-chat-hover cursor-pointer
+					${activeBgClass ? "bg-gray-tertiary" : ""}
+				`}
         onClick={() => setSelectedConversation(conversation)}
       >
         <Avatar className="border border-gray-900 overflow-visible relative">
@@ -39,19 +39,24 @@ const Conversation = ({ conversation }: { conversation: any }) => {
         </Avatar>
         <div className="w-full">
           <div className="flex items-center">
-            <h3 className="text-xs lg:text-sm font-medium">
-              {conversationName}
-            </h3>
-            <span className="text-[10px] lg:text-xs text-gray-500 ml-auto">
+            <h3 className="text-sm font-medium">{conversationName}</h3>
+            <span className="text-xs text-gray-500 ml-auto">
               {formatDate(
                 lastMessage?._creationTime || conversation._creationTime
               )}
             </span>
           </div>
-          <p className="text-[12px] mt-1 text-gray-500 flex items-center gap-1">
-            {lastMessage?.sender === me?._id && <MessageSeenSvg />}
+          <p className="text-[12px] mt-1 text-gray-500 flex items-center gap-1 ">
+            {lastMessage?.sender === me?._id ? <MessageSeenSvg /> : ""}
             {conversation.isGroup && <Users size={16} />}
             {!lastMessage && "Say Hi!"}
+            {lastMessageType === "text" ? (
+              lastMessage?.content.length > 30 ? (
+                <span>{lastMessage?.content.slice(0, 30)}...</span>
+              ) : (
+                <span>{lastMessage?.content}</span>
+              )
+            ) : null}
             {lastMessageType === "image" && <ImageIcon size={16} />}
             {lastMessageType === "video" && <VideoIcon size={16} />}
           </p>
@@ -61,5 +66,4 @@ const Conversation = ({ conversation }: { conversation: any }) => {
     </>
   )
 }
-
 export default Conversation
